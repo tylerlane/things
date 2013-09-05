45ec5558efcc38a85c181d6956703b2775a5f043
Tyler Lane <tyler@nolongervaid.com>
2013-09-04 22:36:21 -0500

    asdf

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,11 @@
 $.index.open();
 
-
+Titanium.App.addEventListener('close', function (e) {
+    Ti.API.info("app exiting. closing the db");
+    if(typeof(db) != "undefined" )
+    {
+        db.close();
+    }
+    
+});     
 
